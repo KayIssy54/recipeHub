b7d7a8e600e7ac84867e060eaa4630213ff8b9de
KayIssy54 <mwongerajoy4@gmail.com>
2026-07-23 22:12:24 +0300

    Fix recipe details page import name in App component

--- recipe-hub/src/App.jsx
+++ recipe-hub/src/App.jsx
@@ -4,7 +4,7 @@ import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import RecipesPage from './pages/RecipesPage';
-import RecipesDetailsPage from './pages/RecipesDetailsPage';
+import RecipeDetailsPage from './pages/RecipeDetailsPage';
 
 function App() {
   return (
@@ -13,11 +13,10 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
 
-      {/* This line is the important one */}
       <Route path="/recipes" element={<RecipesPage />} />
+      <Route path="/recipes/:id" element={<RecipeDetailsPage />} />
 
       <Route path="/categories" element={<h1>Categories Page</h1>} />
-      <Route path="/recipes/:id" element={<Recipe Details Page/>} />
     </Routes>
   );
 }
