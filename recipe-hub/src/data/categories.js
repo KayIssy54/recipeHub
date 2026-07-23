45f9a04583c47af453c1a2bd4f36fb7385cb2063
KayIssy54 <mwongerajoy4@gmail.com>
2026-07-23 22:21:58 +0300

    Add sample category data for categories page

---
+++ recipe-hub/src/data/categories.js
@@ -0,0 +1,46 @@
+const categories = [
+  {
+    id: 1,
+    name: 'Breakfast',
+    image:
+      'https://images.unsplash.com/photo-1525351484163-7529414344d8?w=600',
+    count: 12,
+  },
+  {
+    id: 2,
+    name: 'Lunch',
+    image:
+      'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=600',
+    count: 18,
+  },
+  {
+    id: 3,
+    name: 'Dinner',
+    image:
+      'https://images.unsplash.com/photo-1544025162-d76694265947?w=600',
+    count: 25,
+  },
+  {
+    id: 4,
+    name: 'Dessert',
+    image:
+      'https://images.unsplash.com/photo-1551024601-bec78aea704b?w=600',
+    count: 10,
+  },
+  {
+    id: 5,
+    name: 'Healthy',
+    image:
+      'https://images.unsplash.com/photo-1490645935967-10de6ba17061?w=600',
+    count: 14,
+  },
+  {
+    id: 6,
+    name: 'Snacks',
+    image:
+      'https://images.unsplash.com/photo-1515003197210-e0cd71810b5f?w=600',
+    count: 8,
+  },
+];
+
+export default categories;
