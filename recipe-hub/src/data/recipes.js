8b03928e9be6f87c14b4728001812afa35105a25
KayIssy54 <mwongerajoy4@gmail.com>
2026-07-23 20:39:47 +0300

    Add sample recipe data for frontend development

---
+++ recipe-hub/src/data/recipes.js
@@ -0,0 +1,40 @@
+const recipes = [
+  {
+    id: 1,
+    title: 'Creamy Pasta',
+    category: 'Dinner',
+    rating: 4.8,
+    time: '25 mins',
+    image:
+      'https://images.unsplash.com/photo-1621996346565-e3dbc646d9a9?w=500',
+  },
+  {
+    id: 2,
+    title: 'Chicken Curry',
+    category: 'Lunch',
+    rating: 4.7,
+    time: '40 mins',
+    image:
+      'https://images.unsplash.com/photo-1585937421612-70a008356fbe?w=500',
+  },
+  {
+    id: 3,
+    title: 'Fresh Salad',
+    category: 'Healthy',
+    rating: 4.5,
+    time: '15 mins',
+    image:
+      'https://images.unsplash.com/photo-1546793665-c74683f339c1?w=500',
+  },
+  {
+    id: 4,
+    title: 'Chocolate Cake',
+    category: 'Dessert',
+    rating: 4.9,
+    time: '50 mins',
+    image:
+      'https://images.unsplash.com/photo-1578985545062-69928b1d9587?w=500',
+  },
+];
+
+export default recipes;
