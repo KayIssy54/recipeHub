a145ad8f02b08125928edc0b01a08b205d4ed890
KayIssy54 <mwongerajoy4@gmail.com>
2026-07-31 08:46:54 +0300

    log images

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 import { addFavorite, removeFavorite } from "../services/favorites";
 
 function RecipeCard({ recipe }) {
-  console.log("RECIPE CARD:", recipe);
+  console.log("Title:", recipe.title);
+  console.log("Image:", recipe.image_url);
 
   const [saved, setSaved] = useState(false);
 
